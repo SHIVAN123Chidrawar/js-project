9a4b57b7c79e921bf7ffbdb583bdc92b8642c096
SHIVAN123Chidrawar <53828644+SHIVAN123Chidrawar@users.noreply.github.com>
2019-08-07 00:50:06 +0530

    Add files via upload

---
+++ addition.js
@@ -0,0 +1,13 @@
+var shivani=prompt("enter the exam number")
+var parseInt=shivani
+ var chidrawar=parseInt
+        
+var ruval=prompt("enter the exam number")
+var parseInt=ruval
+var varma=parseInt
+var saro=prompt("enter the exam number")
+var parseInt=saro
+var rout=parseInt
+
+alert(chidrawar*varma*rout)
+ document.write(alert())
